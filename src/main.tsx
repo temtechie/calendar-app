698d3a7a045fbcff36d49295c562ef3be7ef161e
temtechie <templendukwu144@gmail.com>
2023-11-25 18:03:09 +0100

    completed project setup

---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import ErrorPage from './error-page'
+import Root from './routes/root'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
